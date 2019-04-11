2016dc3028f64d6dd1aa42a2b952223c24ce96a9
Laura Ventura Rubio <lauraventurarubio@MacBook-MacBook-Pro-de-Laura-2.local>
2019-04-11 20:10:35 +0200

    pre-modificar-nav

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 // When the user scrolls down 20px from the top of the document, show the button
 //window.onscroll = function() {scrollFunction()};
 
-//function scrollFunction() {
-//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//    document.getElementsByClassName("bottom_dr").style.display = "block";
-//  } else {
-//    document.getElementsByClassName("bottom_dr").style.display = "none";
-//  }
-//}
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementsByClassName("bottom_dr").style.display = "block";
+  } else {
+    document.getElementsByClassName("bottom_dr").style.display = "none";
+  }
+}
 
 // When the user clicks on the button, scroll to the top of the document
-//function topFunction() {
-//  document.body.scrollTop = 0; // For Safari
-//  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-//}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var top =  $(".bottom_dr");
-        if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
-  top.animate({"margin-left": "0px"},1500);
-        } else {
-            top.animate({"margin-left": "-100%"},1500);
-        }
-    });
+//$(document).ready(function () {
+//  $(window).scroll(function () {
+//    var top =  $(".bottom_dr");
+//        if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
+//  top.animate({"margin-left": "0px"},1500);
+//        } else {
+//            top.animate({"margin-left": "-100%"},1500);
+//        }
+//    });
 
-    $(".bottom_dr").on('click', function () {
-        $("html, body").animate({scrollTop: 0}, 400);
-    });
-});
+//    $(".bottom_dr").on('click', function () {
+//        $("html, body").animate({scrollTop: 0}, 400);
+//    });
+//});
