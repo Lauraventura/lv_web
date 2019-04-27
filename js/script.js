4093b40d28643bd6e0bbb8db235f818cea6b14d4
Laura Ventura Rubio <lauraventurarubio@MacBook-MacBook-Pro-de-Laura-2.local>
2019-04-27 18:08:59 +0200

    on_click

--- js/script.js
+++ js/script.js
@@ -4,17 +4,7 @@ document.addEventListener('scroll', function(){
 
   if (document.getElementById('wrapper').scrollTop === 0) {
     element.classList.add('bottom_dr-hidden')
-    //clearInterval(a)
   }
 })
 
 
-//function descreaseScroll() {
-//  console.log('HOLA')
-//  var element = document.getElementById('wrapper')
-//  element.scrollTop -= 10
-//}
-// When the user clicks on the button, scroll to the top of the document
-//function topFunction() {
-//  var a = setInterval(descreaseScroll, 1)
-//}
