b1e768142294dd24fd378a2926c27cc8a87f6aa8
Laura Ventura Rubio <lauraventurarubio@MacBook-MacBook-Pro-de-Laura-2.local>
2019-04-27 12:24:49 +0200

    scroll_iniciresponsive

--- js/script.js
+++ js/script.js
@@ -1,31 +1,20 @@
-// When the user scrolls down 20px from the top of the document, show the button
-//window.onscroll = function() {scrollFunction()};
+document.addEventListener('scroll', function(){
+  var element = document.getElementById('bottom_dr')
+  element.classList.remove('bottom_dr-hidden')
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementsByClassName("bottom_dr").style.display = "block";
-  } else {
-    document.getElementsByClassName("bottom_dr").style.display = "none";
+  if (document.getElementById('wrapper').scrollTop === 0) {
+    element.classList.add('bottom_dr-hidden')
+    //clearInterval(a)
   }
-}
+})
 
+
+//function descreaseScroll() {
+//  console.log('HOLA')
+//  var element = document.getElementById('wrapper')
+//  element.scrollTop -= 10
+//}
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//$(document).ready(function () {
-//  $(window).scroll(function () {
-//    var top =  $(".bottom_dr");
-//        if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
-//  top.animate({"margin-left": "0px"},1500);
-//        } else {
-//            top.animate({"margin-left": "-100%"},1500);
-//        }
-//    });
-
-//    $(".bottom_dr").on('click', function () {
-//        $("html, body").animate({scrollTop: 0}, 400);
-//    });
-//});
+//function topFunction() {
+//  var a = setInterval(descreaseScroll, 1)
+//}
